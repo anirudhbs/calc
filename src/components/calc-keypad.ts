e055a4186b0b1567f03da0cdd20ccba34c6ef499
anirudhbs <anirudhbs96@gmail.com>
2019-04-20 21:19:42 +0530

    CSS cleanup

--- src/components/calc-keypad.ts
+++ src/components/calc-keypad.ts
@@ -27,33 +27,22 @@ export class CalcKeypad extends LitElement {
 
   static get styles() {
     return css`
+      .row {
+        display: block;
+      }
+
       button {
-        height: 3em;
-        width: 8em;
-        margin-top: 0.5em;
-        margin-left: 0.5em;
-      }
-
-      .operator {
-        background: #536ea5;
-        margin-right: 0;
-      }
-
-      .operator:hover {
-        background: #5d7bba;
-        color: #ffffff;
-      }
-
-      .keys button {
-        background: #523d98;
+        background: #4527a0;
         font-size: 27px;
         cursor: pointer;
         width: 3em;
         height: 2em;
+        margin-top: 0.5em;
+        margin-left: 0.5em;
         border-radius: 5px;
         box-shadow: 0px 4px rgba(0, 0, 0, 0.2);
         margin: 0 7px 11px 0;
-        color: #f1c851;
+        color: #ffeb3b;
         line-height: 46px;
         text-align: center;
         user-select: none;
@@ -61,67 +50,55 @@ export class CalcKeypad extends LitElement {
         transition: all 0.2s ease;
       }
 
-      .keys button:hover {
+      button:hover {
         transform: scale(1.05);
       }
 
-      .eval {
+      .special {
         background: #d6643f;
         box-shadow: 0px 4px #c3502b;
         color: #f1c851;
-      }
-
-      .eval:hover {
-        background: #f37c4c;
-        box-shadow: 0px 4px #d65f3d;
-        color: #ffffff;
-      }
-
-      .clear {
-        background: #523d98;
-        box-shadow: 0px 4px rgba(0, 0, 0, 0.2);
-      }
-
-      .clear:hover {
-        background: #f68991;
-        box-shadow: 0px 4px #d3545d;
-        color: white;
       }
     `
   }
 
   render() {
     return html`
-      <div class="keys">
-        <button class="operator" name="(" @click="${this.handleClick}">(</button>
-        <button name="CE" @click="${this.clear}" class="clear">CE</button>
-        <button name=")" @click="${this.handleClick}">)</button>
-        <button name="C" @click="${this.clear}" class="clear">C</button>
-        <br />
+      <div>
+        <div class="row">
+          <button name="(" @click="${this.handleClick}">(</button>
+          <button name=")" @click="${this.handleClick}">)</button>
+          <button name="C" @click="${this.clear}" class="special">C</button>
+          <button name="CE" @click="${this.clear}" class="special">CE</button>
+        </div>
 
-        <button name="1" @click="${this.handleClick}">1</button>
-        <button name="2" @click="${this.handleClick}">2</button>
-        <button name="3" @click="${this.handleClick}">3</button>
-        <button name="+" @click="${this.handleClick}">+</button>
-        <br />
+        <div class="row">
+          <button name="1" @click="${this.handleClick}">1</button>
+          <button name="2" @click="${this.handleClick}">2</button>
+          <button name="3" @click="${this.handleClick}">3</button>
+          <button name="+" @click="${this.handleClick}">+</button>
+        </div>
 
-        <button name="4" @click="${this.handleClick}">4</button>
-        <button name="5" @click="${this.handleClick}">5</button>
-        <button name="6" @click="${this.handleClick}">6</button>
-        <button name="-" @click="${this.handleClick}">-</button>
-        <br />
+        <div class="row">
+          <button name="4" @click="${this.handleClick}">4</button>
+          <button name="5" @click="${this.handleClick}">5</button>
+          <button name="6" @click="${this.handleClick}">6</button>
+          <button name="-" @click="${this.handleClick}">-</button>
+        </div>
 
-        <button name="7" @click="${this.handleClick}">7</button>
-        <button name="8" @click="${this.handleClick}">8</button>
-        <button name="9" @click="${this.handleClick}">9</button>
-        <button name="*" @click="${this.handleClick}">x</button>
-        <br />
+        <div class="row">
+          <button name="7" @click="${this.handleClick}">7</button>
+          <button name="8" @click="${this.handleClick}">8</button>
+          <button name="9" @click="${this.handleClick}">9</button>
+          <button name="*" @click="${this.handleClick}">x</button>
+        </div>
 
-        <button name="." @click="${this.handleClick}">.</button>
-        <button name="0" @click="${this.handleClick}">0</button>
-        <button name="=" @click="${this.computeValue}" class="eval">=</button>
-        <button name="/" @click="${this.handleClick}">÷</button>
-        <br />
+        <div class="row">
+          <button name="." @click="${this.handleClick}">.</button>
+          <button name="0" @click="${this.handleClick}">0</button>
+          <button name="=" @click="${this.computeValue}" class="special">=</button>
+          <button name="/" @click="${this.handleClick}">÷</button>
+        </div>
       </div>
     `
   }
