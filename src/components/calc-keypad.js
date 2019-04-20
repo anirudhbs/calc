9e0ac1bab331fef8fcf301e84d7cc20ec662c2e3
anirudhbs <anirudhbs96@gmail.com>
2019-04-20 17:03:42 +0530

    Get basic functionality working

--- src/components/calc-keypad.js
+++ src/components/calc-keypad.js
@@ -3,19 +3,44 @@ import { LitElement, html } from "lit-element"
 class CalcKeypad extends LitElement {
   constructor() {
     super()
+    this.resetOnInput = true
   }
 
   handleClick(e) {
-    console.log(e.target.name)
+    const resultNode = document.querySelector("#result")
+    if (this.resetOnInput) {
+      resultNode.textContent = ""
+    }
+
+    resultNode.textContent = resultNode.textContent + e.target.name
+    this.resetOnInput = false
+  }
+
+  computeValue() {
+    const resultNode = document.querySelector("#result")
+    resultNode.textContent = eval(resultNode.textContent)
+    this.resetOnInput = true
+  }
+
+  clear() {
+    document.querySelector("#result").textContent = ""
   }
 
   render() {
     return html`
+      <style>
+        button {
+          height: 3em;
+          width: 8em;
+          margin-top: 0.5em;
+          margin-left: 0.5em;
+        }
+      </style>
       <div className="button">
         <button name="(" @click="${this.handleClick}">(</button>
-        <button name="CE" @click="${this.handleClick}">CE</button>
+        <button name="CE" @click="${this.clear}">CE</button>
         <button name=")" @click="${this.handleClick}">)</button>
-        <button name="C" @click="${this.handleClick}">C</button><br />
+        <button name="C" @click="${this.clear}">C</button><br />
 
         <button name="1" @click="${this.handleClick}">1</button>
         <button name="2" @click="${this.handleClick}">2</button>
@@ -34,7 +59,7 @@ class CalcKeypad extends LitElement {
 
         <button name="." @click="${this.handleClick}">.</button>
         <button name="0" @click="${this.handleClick}">0</button>
-        <button name="=" @click="${this.handleClick}">=</button>
+        <button name="=" @click="${this.computeValue}">=</button>
         <button name="/" @click="${this.handleClick}">÷</button><br />
       </div>
     `
